0e4f8a5dbc3a699ae38650dcfdf00c6fb08b0c5e
J. Randall Hunt <randallhunt@gmail.com>
2015-12-04 00:53:52 -0800

    what the actual fuck was I smoking when I wrote this?

--- snap.sh
+++ snap.sh
@@ -6,11 +6,11 @@ latituderef="N"
 if [[ ${latitude:0:1} == "-" ]] ; then
 	latituderef="S"
 fi
+longitude=$(/Users/jhunt3/bin/CoreLocationCLI -once yes -format '%longitude')
 longituderef="W"
-if [[ ${latitude:0:1} == "-" ]] ; then
+if [[ ${longitude:0:1} == "-" ]] ; then
 	longituderef="W"
 fi
-longitude=$(/Users/jhunt3/bin/CoreLocationCLI -once yes -format '%longitude')
 /usr/local/bin/exiftool -q -exif:gpslatitude="$latitude" -exif:gpslatituderef=$latituderef \
             -exif:gpslongitude="$longitude" -exif:gpslongituderef=$longituderef \
             /tmp/snapshot.jpg
